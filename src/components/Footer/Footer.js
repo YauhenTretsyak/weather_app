e125414b21388197c56e9987b0a479401b667683
Yauhen <zhendos@tut.by>
2022-02-18 15:00:06 +0100

    modified some styles in Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,19 @@
 import styled from 'styled-components';
+import screen_breakpoints from '../../styles/StyledElements/screen_breakpoints';
 import { SectionContainer } from '../../styles/StyledElements';
 
 const FooterSection = styled(SectionContainer)``
 
-const Copyrights = styled.p``
+const Copyrights = styled.p`
+  text-align: center;
+
+  ${ screen_breakpoints.md } {
+    text-align: left;
+  }
+`
 
 const AuthorLink = styled.a`
+  display: inline-block;
   padding-left: .9rem;
   text-decoration: none;
   font-family: 'Indie Flower', sans-serif;
